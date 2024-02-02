275859b3b3e19ec552fbb8496d9fc785eb769f99
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-02 22:06:11 +0700

    add bootstrap, membuat list blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
+import { useState } from "react";
 function App() {
-  const [developer, setdeveloper] = useState('Jimmy');
-
-  const handleClick = () =>{
-    setdeveloper('@wiraarbaa_');
-  }
-
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      title:"blog Pertama",
+      body:'Halo ini blog pertma aku',
+      author:"Jimmy Wira Arbaa",
+    },
+    {
+      id: 2,
+      title:"blog Kedua",
+      body:'Halo ini blog pertma aku',
+      author:"Jimmy Wira Arbaa",
+    },
+    {
+      id: 3,
+      title:"blog Ketiga",
+      body:'Halo ini blog pertma aku',
+      author:"Jimmy Wira Arbaa",
+    },
+  ]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Blog with {developer}
-        </p>
-        <button onClick={handleClick}>Show Ig</button>
-      </header>
-    </div>
+    <div className="container mt-3">
+      <h1 className="text-primary">Hello World</h1>
+        {blogs.map((blog, index) => (
+          <div className="card mt-3 p-3">
+            <h3>{blog.title}</h3>
+            <p className="text-muted fst-italic fs-6">Posted by{blog.author}</p>
+            <p className="text-muted">{blog.body}</p>
+          </div>
+        ))}
+      </div>
   );
 }
 
