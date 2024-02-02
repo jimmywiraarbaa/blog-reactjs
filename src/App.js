c2e4dc5f1f06bf4beb72f67db8aae4561351b443
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-02 21:01:41 +0700

    useState pada ReactJS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [developer, setdeveloper] = useState('Jimmy');
+
+  const handleClick = () =>{
+    setdeveloper('@wiraarbaa_');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Blog with {developer}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Show Ig</button>
       </header>
     </div>
   );
