19c662a93955a6166d54602f0614325cb6bad72d
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-07 00:17:29 +0700

    conditional loading and error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import axios from "axios";
 function App() {
   const title = "Jim Kun";
   const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const handleDelete = (id)=>{
     const newBlog = blogs.filter((blog) => blog.id !== id);
@@ -14,17 +16,22 @@ function App() {
   };
 
   useEffect(() => {
-     const fetchData = async () =>{
+    const fetchData = async () =>{
         try {
           const response = await axios.get("http://localhost:8000/blogs");
           console.log(response.data);
           setBlogs(response.data);
+          setLoading(false);
+          setError(false);
         } catch (error) {
           console.log(error);
+          setError(true);
+          setLoading(false);
         }
-     };
-
-     fetchData();
+    };
+    setTimeout(() => {
+      fetchData();
+    }, 2000);
   }, []);
   
   
@@ -33,9 +40,15 @@ function App() {
       <Navbar brand={title}/>
       <div className="container mt-3">
       <h1 className="text-primary">Hello World</h1>
-        {blogs && (
-          <ItemBlog blogs={blogs} handleDelete={handleDelete}/>
-        )}
+      {error &&(
+        <h1 className="text-danger">Error when get data</h1>
+      )}
+      {loading &&(
+        <h1 className="text-muted">Loading...</h1>
+      )}
+      {blogs && (
+        <ItemBlog blogs={blogs} handleDelete={handleDelete}/>
+      )}
       </div>
     </div>
   );
