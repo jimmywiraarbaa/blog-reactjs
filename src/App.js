156dcc74611d0d6961ae892e1e04868ea59ebbe2
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-06 23:48:14 +0700

    json server & load api dari useEffect

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import ItemBlog from "./blog/ItemBlog";
 
+import axios from "axios";
+
 function App() {
   const title = "Jim Kun";
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title:"blog Pertama",
-      body:'Halo ini blog pertma aku',
-      author:"Jimmy Wira Arbaa",
-    },
-    {
-      id: 2,
-      title:"blog Kedua",
-      body:'Halo ini blog pertma aku',
-      author:"Jimmy Wira Arbaa",
-    },
-    {
-      id: 3,
-      title:"blog Ketiga",
-      body:'Halo ini blog pertma aku',
-      author:"Jimmy Wira Arbaa",
-    },    
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id)=>{
     const newBlog = blogs.filter((blog) => blog.id !== id);
@@ -31,9 +14,18 @@ function App() {
   };
 
   useEffect(() => {
-    console.log("Trig Use Effect");
-    console.log(blogs);
-  })
+     const fetchData = async () =>{
+        try {
+          const response = await axios.get("http://localhost:8000/blogs");
+          console.log(response.data);
+          setBlogs(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+     };
+
+     fetchData();
+  }, []);
   
   
   return (
@@ -41,7 +33,9 @@ function App() {
       <Navbar brand={title}/>
       <div className="container mt-3">
       <h1 className="text-primary">Hello World</h1>
-        <ItemBlog blogs={blogs} handleDelete={handleDelete}/>
+        {blogs && (
+          <ItemBlog blogs={blogs} handleDelete={handleDelete}/>
+        )}
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 const navbar = (props) => {
     return ( 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container">
-    <a className="navbar-brand" href="#">{props.brand}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav ms-auto">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Add Blog</a>
-        <a className="nav-link" href="#">Profile</a>
-      </div>
-    </div>
-  </div>
-</nav>
+        <nav className="navbar navbar-expand-lg bg-dark-subtle">
+          <div className="container">
+            <a className="navbar-brand" href="#">{props.brand}</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav ms-auto">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link" href="#">Add Blog</a>
+                <a className="nav-link" href="#">Profile</a>
+              </div>
+            </div>
+          </div>
+        </nav>
      );
 }
 
