72aed8077731400a5ecc43b742f03dc1570717ac
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-07 01:25:06 +0700

    implementasi link pada react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+
 const navbar = (props) => {
     return ( 
         <nav className="navbar navbar-expand-lg bg-dark-subtle">
           <div className="container">
-            <a className="navbar-brand" href="#">{props.brand}</a>
+            <Link to='/' className="navbar-brand">
+            {props.brand}
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav ms-auto">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                <a className="nav-link" href="#">Add Blog</a>
-                <a className="nav-link" href="#">Profile</a>
+                <Link to='/' className="nav-link active" aria-current="page">Home</Link>
+                <Link to='/add' className="nav-link">Add Blog</Link>
+                <Link to='/profile' className="nav-link">Profile</Link>
               </div>
             </div>
           </div>
