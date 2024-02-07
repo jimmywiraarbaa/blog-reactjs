eb6d9129c2b3f852ce152c48301db93848a729a0
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-07 16:46:38 +0700

    custom hooks pada detail blog

--- src/pages/blog/DetailBlog.js
+++ src/pages/blog/DetailBlog.js
@@ -1,10 +1,30 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const DetailBlog = () => {
     const { id } = useParams();
+    const {
+        data: blog,
+        loading,
+        error,
+    } = useFetch("http://localhost:8000/blogs/" + id);
+
     return (
         <div className="container mt-5">
-            <h1 className="text-primary">Detail Blog ke-{id}</h1>
+            <h1 className="text-primary">Detail Blog ke</h1>
+            {error && (
+                <h1 className="text-danger">Error when get data</h1>
+            )}
+            {loading && (
+                <h1 className="text-muted">Loading...</h1>
+            )}
+            {blog && (
+                <div>
+                    <h1 className="mt-3 mb-3">{blog.title}</h1>
+                    <p className="mt-3 mb-3">{blog.body}</p>
+                    <p className="mt-3 mb-3 fst-italic text-muted">posted by {blog.author}</p>
+                </div>
+            )}
         </div>
     );
 }
